c28e14692c42d85e6d4ed6c1728f6b69e0bc9db9
alana.rae.smith@gmail.com <alana.rae.smith@gmail.com>
2017-09-05 19:18:40 -0500

    Understanding what require is doing/replacing from client-side code

---
+++ flag.js
@@ -0,0 +1,7 @@
+const mychalk = require('chalk');
+
+// <script src='node_modules/bla/bla/chalk.js'></script>
+
+// The above code (let mychalk = require....) is essentially replacing the scripts (line 3) you used to 
+// use in your index.html (client-side)
+
